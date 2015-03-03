05ec720f77fe68b6cf1679a94ebb79463fd35233
Lol4to <sidorov@satissoft.ru>
2015-03-03 15:56:07 +0300

    add events to supervising tree

--- src/active_sup.erl
+++ src/active_sup.erl
@@ -5,5 +5,12 @@
 -define(CHILD(I, Type, Args), {I, {I, start_link, Args}, permanent, 5000, Type, [I]}).
 
 start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
-init([]) -> {ok, { {one_for_one, 5, 10}, [?CHILD(active, worker, [])]} }.
+init([]) -> 
+    {ok, { 
+        {one_for_one, 5, 10}, 
+        [
+            ?CHILD(active, worker, []), 
+            ?CHILD(active_events, worker, [])
+        ]
+    }}.
 
