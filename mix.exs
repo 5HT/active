8a0db93103931ff590dfa1ef42cb4c1122f42a9a
Namdak Tonpa <maxim@synrc.com>
2015-03-24 22:31:50 +0200

    https://hex.pm/packages/active

---
+++ mix.exs
@@ -0,0 +1,16 @@
+defmodule Active.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :active,
+     version: "0.9.0",
+     description: "Erlang active reloader",
+     package: package]
+  end
+
+  defp package do
+    [files: ~w(src LICENSE package.exs README.md rebar.config),
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/synrc/active"}]
+   end
+end
