e23658c5c45e73ac7ec01677ffe63c210a3c7631
Namdak Tonpa <maxim@synrc.com>
2018-10-21 22:51:24 +0300

    fix

--- src/active.erl
+++ src/active.erl
@@ -4,6 +4,8 @@
 -export([on_compile/2, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -record(state, {last, root}).
 
+start_link() -> gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
 init([]) ->
     fs:subscribe(),
     erlang:process_flag(priority, low),
